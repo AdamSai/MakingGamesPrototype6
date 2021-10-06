60f7a61c8c00e1c6425ce9987ed2eae36de581c7
Adam Saidane <adamsaidane252@gmail.com>
2021-10-06 19:44:50 +0200

    Added Kelsey's sound files. Added music notes to the animal prefabs

--- Assets/AnimalController.cs
+++ Assets/AnimalController.cs
@@ -5,102 +5,105 @@ using UnityEngine.UI;
 
 public class AnimalController : MonoBehaviour
 {
-  [SerializeField] private float fallSpeed = 10f;
-  [SerializeField] private Animator animalAnimator;
-  [SerializeField] private GameObject balloons;
-  [SerializeField] private Animator balloonAnimator;
-  private AudioSource audioSource;
-  private AnimalNote note;
+    [SerializeField] private float fallSpeed = 10f;
+    [SerializeField] private Animator animalAnimator;
+    [SerializeField] private GameObject balloons;
+    [SerializeField] private Animator balloonAnimator;
+    public AudioSource audioSource {get; private set;  }
+    private AnimalNote note;
 
-  private Button button;
-  private bool isFalling;
+    private Button button;
+    private bool isFalling;
 
-  private Collider2D airTrigger;
-  public string noteName;
+    private Collider2D airTrigger;
+    public string noteName;
 
-  // Start is called before the first frame update
-  void Awake()
-  {
-    audioSource = GetComponent<AudioSource>();
-    animalAnimator = GetComponentInChildren<Animator>();
-    balloonAnimator = balloons.GetComponent<Animator>();
-    note = new AnimalNote(noteName, NotesConfig.notes[noteName]);
-  }
-
-  // Update is called once per frame
-  void Update()
-  {
-    if (isFalling)
+    // Start is called before the first frame update
+    void Awake()
     {
-      transform.position += Vector3.down * fallSpeed * Time.deltaTime;
+        audioSource = GetComponent<AudioSource>();
+        animalAnimator = GetComponentInChildren<Animator>();
+        balloonAnimator = balloons.GetComponent<Animator>();
+        note = new AnimalNote(noteName, NotesConfig.notes[noteName]);
     }
-  }
 
-  private void OnTriggerEnter2D(Collider2D collision)
-  {
-    if (collision.CompareTag("AirTrigger"))
+    // Update is called once per frame
+    void Update()
     {
-      isFalling = false;
-      airTrigger = collision;
-      GamerManager.singingAnimals++;
+        if (isFalling)
+        {
+            transform.position += Vector3.down * fallSpeed * Time.deltaTime;
+        }
     }
-    else if (collision.CompareTag("GroundTrigger"))
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-      LandAnimal();
+        if (collision.CompareTag("AirTrigger"))
+        {
+            isFalling = false;
+            airTrigger = collision;
+            GamerManager.singingAnimals++;
+        }
+        else if (collision.CompareTag("GroundTrigger"))
+        {
+            LandAnimal();
+        }
     }
-  }
 
-  public AnimalNote getNote()
-  {
-    return this.note;
-  }
+    public AnimalNote getNote()
+    {
+        return this.note;
+    }
 
-  /// <summary>
-  /// Call this on badly composed notes
-  /// </summary>
-  public void PopBalloons()
-  {
-    balloonAnimator.Play("Pop");
-    fallSpeed += 10f;
-    Physics2D.IgnoreCollision(airTrigger, GetComponent<BoxCollider2D>());
-    isFalling = true;
-  }
-  /// <summary>
-  /// Call this on succesfully composed notes
-  /// </summary>
-  public void KeepBalloons()
-  {
-    isFalling = true;
-    Physics2D.IgnoreCollision(airTrigger, GetComponent<BoxCollider2D>());
-  }
+    /// <summary>
+    /// Call this on badly composed notes
+    /// </summary>
+    public void PopBalloons()
+    {
+        audioSource.Stop();
+        balloonAnimator.Play("Pop");
+        fallSpeed += 10f;
+        Physics2D.IgnoreCollision(airTrigger, GetComponent<BoxCollider2D>());
+        isFalling = true;
+    }
+    /// <summary>
+    /// Call this on succesfully composed notes
+    /// </summary>
+    public void KeepBalloons()
+    {
+        audioSource.Stop();
+        isFalling = true;
+        Physics2D.IgnoreCollision(airTrigger, GetComponent<BoxCollider2D>());
+    }
 
-  // Teleport animal and make it float downwards
-  public void SelectAnimal(Vector3 fallPosition)
-  {
-    isFalling = true;
-    animalAnimator.enabled = true;
-    transform.position = fallPosition;
-    // TODO: Spawn balloons
-    // TODO: Play sound
+    // Teleport animal and make it float downwards
+    public void SelectAnimal(Vector3 fallPosition)
+    {
+        audioSource.Play();
+        isFalling = true;
+        animalAnimator.enabled = true;
+        transform.position = fallPosition;
+        // TODO: Spawn balloons
+        // TODO: Play sound
 
 
-  }
+    }
 
-  private void LandAnimal()
-  {
-    isFalling = false;
-    // TODO: Pop balloons
-    animalAnimator.enabled = false;
-    GamerManager.landedAnimals++;
-  }
+    private void LandAnimal()
+    {
+        isFalling = false;
+        // TODO: Pop balloons
+        animalAnimator.enabled = false;
+        GamerManager.landedAnimals++;
+    }
 
-  public void PlaySound()
-  {
-    audioSource.Play();
-  }
+    public void PlaySound()
+    {
+        audioSource.Play();
+    }
 
-  public void StopSound()
-  {
-    audioSource.Stop();
-  }
+    public void StopSound()
+    {
+        audioSource.Stop();
+    }
 }
--- Assets/GamerManager.cs
+++ Assets/GamerManager.cs
@@ -6,103 +6,110 @@ using UnityEngine.UI;
 public class GamerManager : MonoBehaviour
 {
 
-  [SerializeField] private GameObject[] Animals = new GameObject[10];
+    [SerializeField] private GameObject[] Animals = new GameObject[10];
 
-  [SerializeField] private Transform[] FallLocations = new Transform[3];
-  [SerializeField] private Button[] animalButtons = new Button[10];
-  [SerializeField] private int maxAllowedAnimals = 3;
-  [SerializeField] private float endOfRoundWaitTime = 3;
-  [SerializeField] private float singingTime = 3;
+    [SerializeField] private Transform[] FallLocations = new Transform[3];
+    [SerializeField] private Button[] animalButtons = new Button[10];
+    [SerializeField] private int maxAllowedAnimals = 3;
+    [SerializeField] private float endOfRoundWaitTime = 3;
+    [SerializeField] private float singingTime = 3;
 
-  public GameObject[] spawnedAnimals = new GameObject[3];
+    public GameObject[] spawnedAnimals = new GameObject[3];
 
-  public static int nextPos = 0; // Next position to spawn an animal from
+    public static int nextPos = 0; // Next position to spawn an animal from
 
-  public static int landedAnimals = 0;
-  public static int singingAnimals = 0;
-  public Transform heartsLocation;
-  public GameObject heartsPrefab;
+    public static int landedAnimals = 0;
+    public static int singingAnimals = 0;
+    public Transform heartsLocation;
+    public GameObject heartsPrefab;
 
-  private void Update()
-  {
-
-    if (landedAnimals == 3)
-    {
-      // TODO: Do what happens when 3 animals have landed
-      StartCoroutine(EndRound());
-    }
-    if (singingAnimals == 3)
-    {
-      StartCoroutine(CompareNotes());
-    }
-  }
-
-  private IEnumerator EndRound()
-  {
-    landedAnimals = 0;
-    // TODO: Play sounds? 
-    yield return new WaitForSeconds(endOfRoundWaitTime);
-
-    foreach (var animal in spawnedAnimals)
-      Destroy(animal);
-    foreach (var button in animalButtons)
-    {
-      button.gameObject.SetActive(true);
-    }
-    nextPos = 0;
-  }
-
-  private IEnumerator CompareNotes()
-  {
-    singingAnimals = 0;
-    bool isRight = isChordRight();
-    if (isRight)
-    {
-      GameObject hearts = Instantiate(heartsPrefab, heartsLocation);
-      Destroy(hearts, 3);
-    }
-    yield return new WaitForSeconds(singingTime);
-
-
-    foreach (GameObject animal in spawnedAnimals)
-    {
-      if (isRight)
-        animal.GetComponent<AnimalController>().KeepBalloons();
-      else
-        animal.GetComponent<AnimalController>().PopBalloons();
-
-    }
-  }
-
-  public void SelectAnimal(int id)
-  {
-    if (nextPos != maxAllowedAnimals)
+    private void Update()
     {
 
-      animalButtons[id].gameObject.SetActive(false);
-      GameObject animal = Instantiate(Animals[id]);
-      spawnedAnimals[nextPos] = animal;
-      animal.GetComponent<AnimalController>().SelectAnimal(FallLocations[nextPos++].position);
+        if (landedAnimals == 3)
+        {
+            // TODO: Do what happens when 3 animals have landed
+            StartCoroutine(EndRound());
+        }
+        if (singingAnimals == 3)
+        {
+            StartCoroutine(CompareNotes());
+        }
     }
 
-  }
-
-  public bool isChordRight()
-  {
-    //TODO: Loop through spawnedAnimals and compare notes
-    foreach (GameObject obj in spawnedAnimals)
+    private IEnumerator EndRound()
     {
-      if (obj.GetComponent<AnimalController>() == null)
-        return false;
+        landedAnimals = 0;
+        // TODO: Play sounds? 
+
+        yield return new WaitForSeconds(endOfRoundWaitTime);
+
+        foreach (var animal in spawnedAnimals)
+            Destroy(animal);
+        foreach (var button in animalButtons)
+        {
+            button.gameObject.SetActive(true);
+        }
+        nextPos = 0;
     }
 
-    AnimalNote keyNote = spawnedAnimals[0].GetComponent<AnimalController>().getNote();
-    if (!keyNote.isCompatible(spawnedAnimals[1].GetComponent<AnimalController>().getNote()))
-      return false;
-    if (!keyNote.isCompatible(spawnedAnimals[2].GetComponent<AnimalController>().getNote()))
-      return false;
-    return true;
-  }
+    private IEnumerator CompareNotes()
+    {
+        singingAnimals = 0;
+        bool isRight = isChordRight();
+        if (isRight)
+        {
+            GameObject hearts = Instantiate(heartsPrefab, heartsLocation);
+            Destroy(hearts, 3);
+        }
+
+        foreach (GameObject animal in spawnedAnimals)
+        {
+            animal.GetComponent<AnimalController>().audioSource.Play();
+        }
+        yield return new WaitForSeconds(singingTime);
+
+
+        foreach (GameObject animal in spawnedAnimals)
+        {
+            animal.GetComponent<AnimalController>().audioSource.Stop();
+            if (isRight)
+                animal.GetComponent<AnimalController>().KeepBalloons();
+            else
+                animal.GetComponent<AnimalController>().PopBalloons();
+
+        }
+    }
+
+    public void SelectAnimal(int id)
+    {
+        if (nextPos != maxAllowedAnimals)
+        {
+
+            animalButtons[id].gameObject.SetActive(false);
+            GameObject animal = Instantiate(Animals[id]);
+            spawnedAnimals[nextPos] = animal;
+            animal.GetComponent<AnimalController>().SelectAnimal(FallLocations[nextPos++].position);
+        }
+
+    }
+
+    public bool isChordRight()
+    {
+        //TODO: Loop through spawnedAnimals and compare notes
+        foreach (GameObject obj in spawnedAnimals)
+        {
+            if (obj.GetComponent<AnimalController>() == null)
+                return false;
+        }
+
+        AnimalNote keyNote = spawnedAnimals[0].GetComponent<AnimalController>().getNote();
+        if (!keyNote.isCompatible(spawnedAnimals[1].GetComponent<AnimalController>().getNote()))
+            return false;
+        if (!keyNote.isCompatible(spawnedAnimals[2].GetComponent<AnimalController>().getNote()))
+            return false;
+        return true;
+    }
 
 
 }
